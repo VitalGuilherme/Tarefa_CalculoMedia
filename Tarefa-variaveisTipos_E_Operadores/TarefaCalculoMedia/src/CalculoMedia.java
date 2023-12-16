c0c02876401d909e4375ad6c8de21e334090e688
Vital.Guilherme <guilhermev.j.santos@gmail.com>
2023-12-16 16:30:03 -0300

    Tarefa_CalculoMedia

---
+++ Tarefa-variaveisTipos_E_Operadores/TarefaCalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,26 @@
+/**
+ * @author VitalGuilherme
+ */
+
+public class CalculoMedia {
+
+   public static void main(String args[]) {
+
+       operaçoesAritmeticas();
+
+
+   }
+    public static void operaçoesAritmeticas() {
+    int bi1 = 5 + 7;
+    int bi2 = 7 + 2;
+    int bi3 = 5 + 8;
+    int bi4 = 4 + 5;
+    int media = (bi1+bi2+bi3+bi4)/4;
+
+    System.out.println("  1 bimestre = " + bi1);
+    System.out.println("  2 bimestre = " + bi2);
+    System.out.println("  3 bimestre = " + bi3);
+    System.out.println("  4 bimestre = " + bi4);
+    System.out.println("  Media = " + media);
+    }
+}
